39af2c858a1f4f7061588acf1d6479903770ac61
͚̤̖͕̪̝͇̟͈̙͉̒͐̎͋͂͛̄̋́̈̇͐̂̋ͅ-̬̰͇̮̟̖̞̬̙̙̯͎͔̣̎̐͋̈́́̒̇̍̑̚H̰͍̙͍̘̘͙͔̊͐̀̀͗̊̚̚Ɔ͖̞͕̭̖̞̠̘̪̫̲̥̣̬͎͈̀̇͋͛͆̔̇̉͑̇̾͌͛̎̍̎Ś͙̯͈͕̱͉̤̭̏͋̃̉͒̑̃̏̾̿̇̃̒͋͌ͅN͉͚̰̫̞̪͈̭̤̞͔͓͎̣̄̓̅̋̈́̉͆̊-̪͈͈̭͎͍̭̥̭͈̣̫̙͍̉̂́̋͆̉̾̄͋̇͌̈̏́̈̚.͓̗̰͙̖̥̠̭̱̙̯̃̀͗͆̃̈́͂͂͐͌͐ <117274957+Nkipohcs@users.noreply.github.com>
2023-12-11 02:47:53 +0100

    Add files via upload

--- Module 01/ex06/main.cpp
+++ Module 01/ex06/main.cpp
@@ -1,12 +1,15 @@
-#include "Karen.hpp"
+#include "Harl.hpp"
+#include <string>
 
-int main() {
-    Karen karen;
+int main(int argc, char **argv) {
+    Harl harl;
 
-    karen.complain("DEBUG");
-    karen.complain("INFO");
-    karen.complain("WARNING");
-    karen.complain("ERROR");
+    if (argc == 2) {
+        std::string level = argv[1];
+        harl.complain(level);
+    } else {
+        std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+    }
 
     return 0;
 }
