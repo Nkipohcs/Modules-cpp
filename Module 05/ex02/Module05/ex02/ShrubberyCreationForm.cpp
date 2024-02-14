3c5951830701b6d957e9ad2d58289ef55647dc9c
͚̤̖͕̪̝͇̟͈̙͉̒͐̎͋͂͛̄̋́̈̇͐̂̋ͅ-̬̰͇̮̟̖̞̬̙̙̯͎͔̣̎̐͋̈́́̒̇̍̑̚H̰͍̙͍̘̘͙͔̊͐̀̀͗̊̚̚Ɔ͖̞͕̭̖̞̠̘̪̫̲̥̣̬͎͈̀̇͋͛͆̔̇̉͑̇̾͌͛̎̍̎Ś͙̯͈͕̱͉̤̭̏͋̃̉͒̑̃̏̾̿̇̃̒͋͌ͅN͉͚̰̫̞̪͈̭̤̞͔͓͎̣̄̓̅̋̈́̉͆̊-̪͈͈̭͎͍̭̥̭͈̣̫̙͍̉̂́̋͆̉̾̄͋̇͌̈̏́̈̚.͓̗̰͙̖̥̠̭̱̙̯̃̀͗͆̃̈́͂͂͐͌͐ <117274957+Nkipohcs@users.noreply.github.com>
2024-02-14 14:19:52 +0100

    Add files via upload

---
+++ Module 05/ex02/Module05/ex02/ShrubberyCreationForm.cpp
@@ -0,0 +1,18 @@
+#include "ShrubberyCreationForm.hpp"
+#include <fstream>
+#include <iostream>
+
+
+ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target)
+: AForm("ShrubberyCreationForm", 145, 137), target(target) {}
+
+void ShrubberyCreationForm::execute(Bureaucrat const & executor) const {
+    if (!getIsSigned() || executor.getGrade() > getGradeRequiredToExecute()) {
+        throw AForm::GradeTooLowException();
+    }
+    std::ofstream ofs((this->target + "_shrubbery").c_str());
+    if (ofs) {
+        ofs << "ASCII trees" << std::endl;
+    }
+    ofs.close();
+}
